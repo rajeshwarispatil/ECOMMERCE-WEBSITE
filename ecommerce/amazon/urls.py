fe2f70da9d21e9fc36b82aa825102466274477de
S <rajeshwarispatil02@gmail.com>
2026-02-16 15:15:56 +0530

    Added gitignore

---
+++ ecommerce/amazon/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('add/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('increase/<int:id>/', views.increase, name='increase'),
+    path('decrease/<int:id>/', views.decrease, name='decrease'),
+    path('remove/<int:id>/', views.remove, name='remove'),
+]
---
+++ ecommerce/amazon/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from .models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
+def add_to_cart(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        cart[str(id)] += 1
+    else:
+        cart[str(id)] = 1
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
+    final_amount = 0
+
+    for id in cart:
+        product = Product.objects.get(id=id)
+        quantity = cart[id]
+        total = product.price * quantity
+        final_amount += total
+
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'total': total
+        })
+
+    return render(request, 'store/cart.html', {
+        'cart_items': cart_items,
+        'final_amount': final_amount
+    })
+
+
+def increase(request, id):
+    cart = request.session.get('cart', {})
+    cart[str(id)] = cart.get(str(id), 0) + 1
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+
+def decrease(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        if cart[str(id)] > 1:
+            cart[str(id)] -= 1
+        else:
+            del cart[str(id)]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+
+def remove(request, id):
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        del cart[str(id)]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
